5650ad05781f1c5802c097a1f285bcd1aa081db7
mrjang1004 <mrjang1004@gmail.com>
2024-04-19 18:30:17 +0800

    upload(complete)

---
+++ Controllers/ShippersController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TimeNowExample;
+
+namespace TimeNowExample.Controllers
+{
+    public class ShippersController : Controller
+    {
+        private NorthWindDBEntities db = new NorthWindDBEntities();
+
+        // GET: Shippers
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Shippers.ToListAsync());
+        }
+
+        // GET: Shippers/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Shippers shippers = await db.Shippers.FindAsync(id);
+            if (shippers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shippers);
+        }
+
+        // GET: Shippers/Create
+        public ActionResult Create()
+        {
+            Shippers shippers = new Shippers();
+            return View(shippers);
+        }
+
+        // POST: Shippers/Create
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ShipperID,CompanyName,Phone")] Shippers shippers)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Shippers.Add(shippers);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(shippers);
+        }
+
+        // GET: Shippers/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Shippers shippers = await db.Shippers.FindAsync(id);
+            if (shippers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shippers);
+        }
+
+        // POST: Shippers/Edit/5
+        // 若要避免過量張貼攻擊，請啟用您要繫結的特定屬性。
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ShipperID,CompanyName,Phone")] Shippers shippers)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(shippers).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(shippers);
+        }
+
+        // GET: Shippers/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Shippers shippers = await db.Shippers.FindAsync(id);
+            if (shippers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shippers);
+        }
+
+        // POST: Shippers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Shippers shippers = await db.Shippers.FindAsync(id);
+            db.Shippers.Remove(shippers);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TimeNowExample.MvcApplication" Language="C#" %>
